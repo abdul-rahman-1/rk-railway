8b0a2e1148a8e35fb75385352e44c17c556b694b
Abdul Rahman <105973330+abdul-rahman-1@users.noreply.github.com>
2025-09-04 07:18:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -168,4 +168,4 @@ def inspect(unique_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.getenv("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)), debug=False)
